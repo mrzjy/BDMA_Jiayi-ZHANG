dfa5336e07674d80abe770dc45f36d2d887acc1e
Jiayi_VM <jiayi.zhang9312@gmail.com>
2017-02-13 10:13:48 -0800

    final

--- InvertIndexExten.java
+++ InvertIndexExten.java
@@ -1,20 +1,19 @@
 package edu.stanford.cs246.wordcount;
 
 import java.io.BufferedReader;
-import java.io.DataOutput;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.PrintWriter;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.conf.Configured;
 import org.apache.hadoop.fs.FileSystem;
 import org.apache.hadoop.fs.Path;
-import org.apache.hadoop.io.ArrayWritable;
-import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Job;
@@ -28,18 +27,21 @@ import org.apache.hadoop.mapreduce.lib.output.TextOutputFormat;
 import org.apache.hadoop.util.Tool;
 import org.apache.hadoop.util.ToolRunner;
 
-public class InvertIndex extends Configured implements Tool {	
-   private enum MyCounter {
+public class InvertIndexExten extends Configured implements Tool {	
+   
+   private enum ONLY_WORD_COUNTER {
 		  PG100,
 		  PG31100,
 		  PG3200
-   }
+		 }
+	
    public static void main(String[] args) throws Exception {
       System.out.println(Arrays.toString(args));
       
       Configuration conf = new Configuration();
       conf.set("StopWordsFileName", args[2]);
-      int res = ToolRunner.run(conf, new InvertIndex(), args);
+//      conf.set("mapreduce.map.output.compress", "true");
+      int res = ToolRunner.run(conf, new InvertIndexExten(), args);
 	  
       
       System.exit(res);
@@ -48,14 +50,15 @@ public class InvertIndex extends Configured implements Tool {
    @Override
    public int run(String[] args) throws Exception {
       System.out.println(Arrays.toString(args));
-      Job job = new Job(getConf(), "InvertIndex");
-      job.setJarByClass(InvertIndex.class);
+      Job job = new Job(getConf(), "InvertIndexExten");
+      job.setJarByClass(InvertIndexExten.class);
       job.setOutputKeyClass(Text.class);
-      job.setOutputValueClass(Text[].class);
+      job.setOutputValueClass(Text.class);
 
       job.setMapperClass(Map.class);
       job.setReducerClass(Reduce.class);
-//      job.setCombinerClass(Reduce.class);
+
+      job.setNumReduceTasks(10);
       
       job.setInputFormatClass(TextInputFormat.class);
       job.setOutputFormatClass(TextOutputFormat.class);
@@ -68,16 +71,6 @@ public class InvertIndex extends Configured implements Tool {
 
       job.waitForCompletion(true);
       
-      Integer i;
-      PrintWriter writer = new PrintWriter(args[3], "UTF-8");
-      i = (int) job.getCounters().findCounter(MyCounter.PG100).getValue();
-      writer.println("PG100: "+i.toString()+"\n");
-      i = (int) job.getCounters().findCounter(MyCounter.PG31100).getValue();
-      writer.println("PG31100: "+i.toString()+"\n");
-      i = (int) job.getCounters().findCounter(MyCounter.PG3200).getValue();
-      writer.println("PG3200: "+i.toString()+"\n");
-      writer.close();
-      
       return 0;
    }
    
@@ -87,20 +80,20 @@ public class InvertIndex extends Configured implements Tool {
       public List<Text> stopWords = new ArrayList<Text>(); 
       
       public void loadStopWords(String filename) throws IOException{
-    	  Path path=new Path(filename);
+    	  Path pt=new Path(filename);//Location of file in HDFS
           FileSystem fs = FileSystem.get(new Configuration());
-    	  BufferedReader BR = new BufferedReader(new InputStreamReader(fs.open(path)));
+    	  BufferedReader br = new BufferedReader(new InputStreamReader(fs.open(pt)));
     		  
     	  String sCurrentLine;
     	      
-          while ((sCurrentLine = BR.readLine()) != null) {
+          while ((sCurrentLine = br.readLine()) != null) {
         	  String stopWord = sCurrentLine.replaceAll("[^A-Za-z]+", "");
-        	  Text text = new Text();
-        	  text.set(stopWord);
-        	  stopWords.add(text);
+        	  Text t = new Text();
+        	  t.set(stopWord);
+        	  stopWords.add(t);
     	  }
           
-          BR.close();
+          br.close();
           
           return;
        }
@@ -116,10 +109,8 @@ public class InvertIndex extends Configured implements Tool {
               throws IOException, InterruptedException {
     	 FileSplit fileSplit = (FileSplit)context.getInputSplit();
     	 String name = fileSplit.getPath().getName();
-    	 //String one = String.valueOf(1);
     	 filename.set(name);
          for (String token: value.toString().split("\\s+|-{2,}+")) {
-        	 
             word.set(token.replaceAll("[^A-Za-z]+", "").toLowerCase());
             if (!stopWords.contains(word)){
                context.write(word, filename);            	
@@ -128,66 +119,24 @@ public class InvertIndex extends Configured implements Tool {
       }
    }
 
-   public static class Reduce extends Reducer<Text, Text, Text, TextArray> {
+   public static class Reduce extends Reducer<Text, Text, Text, Text> {
       @Override
       public void reduce(Text key, Iterable<Text> values, Context context)
               throws IOException, InterruptedException {
-         ArrayList<Text> res = new ArrayList<Text>();
+         List<Text> res = new ArrayList<Text>();
          for (Text val : values) {
-        	
-        	if (!res.contains(val)){
-        		res.add(new Text(val));
-        	}
+        	res.add(new Text(val));
          }
-         if (res.size()==1){
-        	 String filename = res.get(0).toString();
-        	 switch (filename){
-	        	 case "pg100.txt":
-	        		 context.getCounter(MyCounter.PG100).increment(1);
-	        		 break;
-	        	 case "pg31100.txt":
-	        		 context.getCounter(MyCounter.PG31100).increment(1);
-	        		 break;
-	        	 case "pg3200.txt":
-	    		     context.getCounter(MyCounter.PG3200).increment(1);
-	    		     break;
-	         }
+         Set<Text> filenames = new HashSet<Text>(res);
+         String output = new String();
+         for (Text txt : filenames){
+        	 output += txt.toString()+'#'+Collections.frequency(res, txt);
+        	 output += ',';
          }
-         Text[] arr = new Text[res.size()];
-         arr = res.toArray(arr);
-         TextArray output = new TextArray(arr);
-         output.set(arr);
-         context.write(key, output);
+         output = output.substring(0, output.length()-1);
+         Text value = new Text();
+         value.set(output);
+         context.write(key, value);
       }
-   }
-   
-   public static class TextArray extends ArrayWritable {
-	    public TextArray(Text[] arr) {
-	        super(Text.class);
-	    }
-
-	    @Override
-	    public Text[] get() {
-	        return (Text[]) super.get();
-	    }
-
-	    @Override
-	    public void write(DataOutput arg0) throws IOException {
-	        for(Text data : get()){
-	            data.write(arg0);
-	        }
-	    }
-	    
-	    @Override
-	    public String toString() {
-	        Text[] values = get();
-	        String output = new String();
-	        for (Text t: values){
-		        output += t.toString();
-		        output += ",";
-	        }
-	        output = output.substring(0, output.length()-1);
-	        return output;
-	    }
-	}   
+   }   
 }
